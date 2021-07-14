27d6ab9780525cda6b0dec5c648c5a66e6fc54f0
Blake DeGraw <bbdegraw@gmail.com>
2021-07-14 13:56:43 -0700

    alert message pops up when picking more than 5 lis

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState, } from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 // import './App.css'
 import Title from './components/Title'
 import Assemble from './components/Assemble'
+import Battle from './components/Battle'
+import { render } from '@testing-library/react'
 const axios = require('axios')
 // const cors = require('cors')
 // const STAR_WARS_URL = process.env.STAR_WARS_URL
@@ -11,32 +14,40 @@ function App() {
 
   const [ships, setShips] = useState([])
   const [myShips, setMyShips] = useState([])
+  const [redirect, setRedirect] = useState('/battle')
 
   // console.log(myShips)
 
   useEffect(() => {
     axios.get('http://localhost:8000/')
-    .then(shipData => {
-      setShips(shipData.data)
-    })
+      .then(shipData => {
+        setShips(shipData.data)
+      })
   }, [])
 
-  // let testNum = 0
-
-  function addShip(e){
-    // console.log('addShip firing')
-    let tempShips = myShips
-    tempShips.push(e.target.id)
-    setMyShips(tempShips)
-    console.log(myShips)
-    // testNum++
+  function addShip(e) {
+    if (myShips.length < 5) {
+      let tempShips = myShips
+      tempShips.push(e.target.id)
+      setMyShips(tempShips)
+      console.log(myShips)
+    } else {
+      alert("Roster full")
+    }
   }
 
+  // if(redirect){
+
+  // }
+
   return (
-    <Router>
-      <Route exact path='/' render={Title} />
-      <Route path='/assemble' render={() => <Assemble ships={ships} addShip={addShip} /> } />
-    </Router>
+    <div>
+      <Router>
+        <Route exact path='/' render={Title} />
+        <Route path='/assemble' render={() => <Assemble ships={ships} addShip={addShip} />} />
+        <Route  exact path='/battle' render={Battle} />
+      </Router>
+    </div>
   )
 }
 
---
+++ src/components/Battle.js
@@ -0,0 +1,9 @@
+function Battle(){
+    return(
+        <div>
+            <h1>let's do battle!</h1>
+        </div>
+    )
+}
+
+export default Battle

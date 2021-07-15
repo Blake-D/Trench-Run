6a45eb363e9e5c2c285cb3963e758dcc2325b5c7
Blake DeGraw <bbdegraw@gmail.com>
2021-07-15 12:34:24 -0700

    solved the cors issue by making a proxy in package.json. making api call from front-end now, nixing the back end.

--- src/App.js
+++ src/App.js
@@ -11,11 +11,28 @@ function App() {
   const [ships, setShips] = useState([])
   const [myShips, setMyShips] = useState([])
 
+  // useEffect(() => {
+  //   axios.get('http://localhost:8000/')
+  //     .then(shipData => {
+  //       setShips(shipData.data)
+  //     })
+  // }, [])
+
+  // useEffect(() => {
+  //   axios.get('https://swgoh.gg/api/ships')
+  //     .then(shipData => {
+  //       setShips(shipData.data)
+  //     })
+  // }, [])
+
   useEffect(() => {
-    axios.get('http://localhost:8000/')
-      .then(shipData => {
-        setShips(shipData.data)
-      })
+    axios.get('/api/ships')
+    .then(res => console.log(res.data))
+    // .then(res => res.json())
+    // .then(shipData => {
+    //   shipData = Object.values(shipData.data)
+    //   setShips(shipData)
+    // })
   }, [])
 
   console.log(ships)
